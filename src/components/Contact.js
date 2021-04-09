7f0a9211f41aec4c585bd86d1a88d23f5a1a9788
Hungchen Fu <sean14xx@gmail.com>
2021-04-09 13:11:14 +0200

    feat: Add Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>contact</h1>
+      <p>
+        I'm always open to new challenges, <br />
+        so feel free to drop me a line at <br />
+        contact@hungchenfu.com
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Greeting from "./Greeting";
+import Contact from "./Contact";
 
 const Homepage = () => {
   return (
     <div>
       <Greeting />
+      <Contact />
     </div>
   );
 };
