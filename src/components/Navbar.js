dc8163bb10e04167ae546e56ed75b40c4a04ca4a
Hungchen Fu <sean14xx@gmail.com>
2021-04-11 17:42:09 +0200

    feat: Add style to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../assets/img/logo.png";
+import "../stylesheets/navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <a href="/">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" id="logo" />
       </a>
       <ul>
         <li>
