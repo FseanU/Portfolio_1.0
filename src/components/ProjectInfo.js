8cd4c3065a97521f078bc875b35b92cd3c98c2c3
Hungchen Fu <sean14xx@gmail.com>
2021-04-14 16:55:27 +0200

    feat: Add ProjectInfo

---
+++ src/components/ProjectInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectInfo = () => {
+  return (
+    <div className="project-info">
+      <h1>Project title</h1>
+      <p>Year: 2020</p>
+    </div>
+  );
+};
+
+export default ProjectInfo;
