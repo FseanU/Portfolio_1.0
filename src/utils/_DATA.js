b133eec034962e72d625920e57a0a38cabdd38ca
Hungchen Fu <sean14xx@gmail.com>
2021-04-16 14:48:48 +0200

    feat: Add data for Jamin project

---
+++ src/utils/_DATA.js
@@ -0,0 +1,57 @@
+export const projects = {
+  jamin: {
+    id: "jamin",
+    projectInfo: {
+      name: "Jamin",
+      year: "2020",
+    },
+    content: [
+      {
+        imgUrl: "/img/jamin/jammood.jpg",
+        paragraph:
+          "Jamin is a platform that helps connect musicians to play and enjoy music together through jam sessions. Musicians can create jam sessions or simply join one.",
+      },
+      {
+        imgUrl: "/img/jamin/home.png",
+        title: "01 | FIND A JAM SESSION",
+        paragraph:
+          "Based on instrument, genre, and date, Jamin helps users find the best jam session in their area.",
+      },
+      {
+        imgUrl: "/img/jamin/discover.png",
+        title: "02 | DISCOVER",
+        paragraph: "",
+      },
+      {
+        imgUrl: "/img/jamin/join.png",
+        title: "03 | JOIN",
+        paragraph: "",
+      },
+      {
+        imgUrl: "/img/jamin/readytojam.png",
+        title: "04 | READY TO JAM",
+        paragraph:
+          "Users can check the information of upcoming jam session or write a review for the jam session they participated.",
+      },
+    ],
+  },
+  wyr: {
+    id: "wyr",
+    projectInfo: {
+      name: "WYR",
+      year: "2020",
+    },
+    content: [
+      {
+        imgUrl: "",
+        title: "",
+        paragraph: "",
+      },
+      {
+        imgUrl: "",
+        title: "",
+        paragraph: "",
+      },
+    ],
+  },
+};
