ccd6b5e97772b9f8aa0b814ee8409416629d3ae9
Hungchen Fu <sean14xx@gmail.com>
2021-04-20 21:02:17 +0200

    feat: Add style to ProjectPage

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { Link } from "@reach/router";
+import styled from "styled-components";
 import ProjectInfo from "./ProjectInfo";
 import ProjectContent from "./ProjectContent";
 import { projects } from "../utils/_DATA";
+
+const StyledProjectPage = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 12fr 1fr;
+`;
+
+const StyledLink = styled(Link)`
+  grid-column: 2/3;
+  margin: 1rem 0;
+  font-size: 18px;
+`;
 
 const ProjectPage = ({ projectId }) => {
   const { projectInfo, content } = projects[projectId];
 
   return (
-    <div className="project-page">
-      <Link to="/">
-        <p style={{ margin: "1rem 0", fontSize: "18px" }}>{"<< back"}</p>
-      </Link>
+    <StyledProjectPage>
+      <StyledLink to="/">{"<< back"}</StyledLink>
       <ProjectInfo
         projectTitle={projectInfo.name}
         projectYear={projectInfo.year}
       />
       <ProjectContent content={content} />
-    </div>
+    </StyledProjectPage>
   );
 };
 
