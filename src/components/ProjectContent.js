7b7836b5818ec9b0b61321a96563e8ac96f7e80d
Hungchen Fu <sean14xx@gmail.com>
2021-04-20 11:53:40 +0200

    feat: Add grid layout to ProjectContent

--- src/components/ProjectContent.js
+++ src/components/ProjectContent.js
@@ -1,18 +1,30 @@
 import React from "react";
 import ProjectContentBlock from "./ProjectContentBlock";
+import styled from "styled-components";
+
+const StyledProjectContent = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 12fr 1fr;
+  grid-gap: 8rem 0;
+`;
 
 const ProjectContent = ({ content }) => {
   return (
-    <>
-      {content.map(({ imgUrl, title, paragraph }) => (
-        <ProjectContentBlock
-          imgUrl={imgUrl}
-          title={title}
-          paragraph={paragraph}
-          key={imgUrl}
-        />
-      ))}
-    </>
+    <StyledProjectContent>
+      {content.map(({ imgUrl, title, paragraph }, index) => {
+        const textLeft = index % 2 === 0;
+
+        return (
+          <ProjectContentBlock
+            imgUrl={imgUrl}
+            title={title}
+            paragraph={paragraph}
+            key={imgUrl}
+            textLeft={textLeft}
+          />
+        );
+      })}
+    </StyledProjectContent>
   );
 };
 
