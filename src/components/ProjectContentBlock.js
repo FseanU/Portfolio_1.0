bacfa0bd47f7d29ef95bf800d1a2dfbe9cb6e085
Hungchen Fu <sean14xx@gmail.com>
2021-04-20 11:54:28 +0200

    feat: Add style to ProjectContentBlock

--- src/components/ProjectContentBlock.js
+++ src/components/ProjectContentBlock.js
@@ -1,11 +1,55 @@
 import React from "react";
+import styled from "styled-components";
+import TextContent from "./TextContent";
 
-const ProjectContentBlock = ({ imgUrl, title, paragraph }) => (
-  <div>
-    {title ? <h3>{title}</h3> : ""}
-    <p>{paragraph}</p>
-    <img src={process.env.PUBLIC_URL + imgUrl} alt="" />
-  </div>
-);
+const StyledProjectContentBlock = styled.div`
+  grid-column: 2/3;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-gap: 8rem 0;
+  align-items: center;
+`;
+
+const ImgContent = styled.div`
+  grid-column: ${(props) => props.gridColumn};
+`;
+
+const ProjectContentBlock = ({ imgUrl, title, paragraph, textLeft }) => {
+  return (
+    <StyledProjectContentBlock>
+      {textLeft ? (
+        <>
+          <TextContent
+            title={title}
+            paragraph={paragraph}
+            gridColumn={"1 / 5"}
+          />
+          <ImgContent gridColumn={"6 / 13"}>
+            <img
+              src={process.env.PUBLIC_URL + imgUrl}
+              alt=""
+              style={{ width: "100%" }}
+            />
+          </ImgContent>
+        </>
+      ) : (
+        <>
+          <ImgContent gridColumn={"1 / 8"}>
+            <img
+              src={process.env.PUBLIC_URL + imgUrl}
+              alt=""
+              style={{ width: "100%" }}
+            />
+          </ImgContent>
+          <TextContent
+            title={title}
+            paragraph={paragraph}
+            gridColumn={"9 / 13"}
+          />
+        </>
+      )}
+    </StyledProjectContentBlock>
+  );
+};
 
 export default ProjectContentBlock;
