e2c9e1bd7dadb01a8d81747c0b28c857ceaa27d0
Hungchen Fu <sean14xx@gmail.com>
2021-04-21 11:02:51 +0200

    feat: Add style to TextContent

--- src/components/TextContent.js
+++ src/components/TextContent.js
@@ -1,16 +1,28 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 const StyledTextContent = styled.div`
   grid-column: ${(props) => props.gridColumn};
   text-align: ${(props) => props.textAlign};
 `;
 
+const Paragraph = styled.p`
+  font-weight: 300;
+  font-size: calc(14px + 0.5vw);
+  line-height: 1.5;
+`;
+
+const Heading = styled.h3`
+  font-weight: 400;
+  margin-bottom: 2rem;
+  font-size: 24px;
+`;
+
 const TextContent = ({ title, paragraph, gridColumn }) => {
   return (
     <StyledTextContent gridColumn={gridColumn}>
-      {title ? <h3>{title}</h3> : ""}
-      <p>{paragraph}</p>
+      {title ? <Heading>{title}</Heading> : ""}
+      <Paragraph>{paragraph}</Paragraph>
     </StyledTextContent>
   );
 };
