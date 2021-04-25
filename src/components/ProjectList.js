0b62fa5c614df1078c4633b477e9167a80b16b17
Hungchen Fu <sean14xx@gmail.com>
2021-04-25 12:09:03 +0200

    feat: Adjust ProjectList UI

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,11 +5,15 @@ import { projects } from "../utils/_DATA";
 
 const StyledProjectList = styled.div`
   grid-column: 2/3;
-  border-top: solid 0.2rem var(--black);
 `;
 
-const Heading = styled.h3`
-  border-bottom: solid 0.2rem var(--black);
+const Divider = styled.div`
+  grid-column: 2/3;
+  border-bottom: 2px solid var(--black);
+`;
+
+const Title = styled.h3`
+  border-bottom: solid 2px var(--black);
   margin: 0;
   padding: 2rem 0;
   font-size: calc(48px + 1.5vw);
@@ -23,24 +27,27 @@ const Heading = styled.h3`
   &:hover {
     color: transparent;
     -webkit-text-stroke: 1.5px var(--highlight);
-    border-bottom: solid 0.2rem var(--highlight);
+    border-bottom: solid 2px var(--highlight);
   }
 `;
 
 const ProjectList = () => {
   return (
-    <StyledProjectList id="project">
-      {Object.keys(projects).map((projectKey) => {
-        const projectName = projects[projectKey].projectInfo.name;
-        const id = projects[projectKey].id;
+    <>
+      <Divider></Divider>
+      <StyledProjectList id="project">
+        {Object.keys(projects).map((projectKey) => {
+          const projectName = projects[projectKey].projectInfo.name;
+          const id = projects[projectKey].id;
 
-        return (
-          <Link to={`/projects/${id}`}>
-            <Heading>{projectName}</Heading>
-          </Link>
-        );
-      })}
-    </StyledProjectList>
+          return (
+            <Link to={`/projects/${id}`}>
+              <Title>{projectName}</Title>
+            </Link>
+          );
+        })}
+      </StyledProjectList>
+    </>
   );
 };
 
